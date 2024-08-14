e80dc33333723664455daa2e5f3b5f50c56ada5f
Vybec <vybex9@gmail.com>
2024-08-14 22:45:56 +0530

    category list edit btn setuped done boy

--- controllers/admin/categoryController.js
+++ controllers/admin/categoryController.js
@@ -140,5 +140,41 @@ const getUnlistCategory =async(req,res)=>{
     }
 }
 
+const getEditCategory =async (req,res)=>{
+    try {
+        const id=req.query.id;
+        const category =await Category.findOne({_id:id});
+        res.render('edit-category',{category:category});
+    } catch (error) {
+        res.redirect('/pageerror')
+    }
+}
 
-module.exports = { categoryInfo, addCategory ,addCategoryOffer,removeCategoryOffer,getListCategory,getUnlistCategory };
+const editCategory =async(req,res)=>{
+    try {
+        const id=req.params.id;
+        const {categoryName,description} =req.body;
+        const existingCategory = await Category.findOne({name:categoryName});
+        if(existingCategory){
+            return res.status(400).json({error:'Category exists,please choose another name'})
+        }
+
+        const updateCategory = await Category.findByIdAndUpdate(id,{
+            name:categoryName,
+            description:description,
+
+        },{new:true});
+
+        if(updateCategory){
+            res.redirect('/admin/category');
+        }else{
+            res.status(404).json({error:'Category not found'})
+        }
+
+    } catch (error) {
+        res.status(500).json({error:'Internal server error'})
+    }
+}
+
+
+module.exports = { categoryInfo, addCategory ,addCategoryOffer,removeCategoryOffer,getListCategory,getUnlistCategory,getEditCategory,editCategory };
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -27,5 +27,7 @@ router.post('/addCategoryOffer',adminAuth,categoryController.addCategoryOffer);
 router.post('/removeCategoryOffer',adminAuth,categoryController.removeCategoryOffer);
 router.get('/listCategory',adminAuth,categoryController.getListCategory);
 router.get('/unlistCategory',adminAuth,categoryController.getUnlistCategory);
+router.get('/editCategory',adminAuth,categoryController.getEditCategory);
+router.post('/editCategory/:id',adminAuth,categoryController.editCategory);
 
 module.exports =router;
